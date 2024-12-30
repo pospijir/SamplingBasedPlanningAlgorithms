a9dbce65f07b0f4df3f4fa2258444d7f4e53a31a
Jiří Pospíšil <jiri.pospisil262@gmail.com>
2024-12-30 16:51:35 +0100

    Rename plot functions

--- src/plotting.jl
+++ src/plotting.jl
@@ -7,30 +7,30 @@ const AGENT_BORDER_WIDTH = 2
 
 const OBSTACLE_COLOR = :black
 
-function plot!(a::Agent{CircleAgent})
+function plot_agent!(a::Agent{CircleAgent})
     poly!(Circle(Point2f(a.pose.x, a.pose.y), a.radius), color=AGENT_COLOR, strokecolor=AGENT_BORDER_COLOR, strokewidth=AGENT_BORDER_WIDTH)
 end
 
-function plot!(a::Agent{PointAgent}, radius=0.1)
+function plot_agent!(a::Agent{PointAgent}, radius=0.1)
     poly!(Circle(Point2f(a.pose.x, a.pose.y), radius), color=AGENT_COLOR, strokecolor=AGENT_BORDER_COLOR, strokewidth=AGENT_BORDER_WIDTH)
 end
 
-function plot!(a::Agent{PolygonAgent})
+function plot_agent!(a::Agent{PolygonAgent})
     points = Point2f[[(vertex.x, vertex.y) for vertex in a.vertices]...]
     poly!(points, color=OBSTACLE_COLOR)
 end
 
-function plot!(o::Obstacle{CircleObstacle})
+function plot_obstacle!(o::Obstacle{CircleObstacle})
     poly!(Circle(Point2f(o.pose.x, o.pose.y), o.radius), color=OBSTACLE_COLOR)
 end
 
-function plot!(o::Obstacle{O}) where {O <: ObstacleType}
+function plot_obstacle!(o::Obstacle{O}) where {O <: ObstacleType}
     points = Point2f[[(vertex.x, vertex.y) for vertex in o.vertices]...]
     poly!(points, color=OBSTACLE_COLOR)
 end
 
-function plot!(w::World)
+function plot_world!(w::World)
     for o in w.obstacles
-        plot!(o)
+        plot_obstacle!(o)
     end
 end
