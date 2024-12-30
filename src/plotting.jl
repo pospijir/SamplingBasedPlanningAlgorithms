82a156b989fb99154f0553618ff51a3d927ec3e4
Jiří Pospíšil <jiri.pospisil262@gmail.com>
2024-12-30 20:19:43 +0100

    Fix color

--- src/plotting.jl
+++ src/plotting.jl
@@ -17,7 +17,7 @@ end
 
 function plot_agent!(a::Agent{PolygonAgent})
     points = Point2f[[(vertex.x, vertex.y) for vertex in a.vertices]...]
-    poly!(points, color=OBSTACLE_COLOR)
+    poly!(points, color=AGENT_COLOR)
 end
 
 function plot_obstacle!(o::Obstacle{CircleObstacle})
