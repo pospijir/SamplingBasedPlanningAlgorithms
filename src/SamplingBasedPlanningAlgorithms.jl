ee7e26c919c71e4dfdd06584bb913cef79856bf1
Jiří Pospíšil <jiri.pospisil262@gmail.com>
2024-12-30 16:27:25 +0100

    Export world

--- src/SamplingBasedPlanningAlgorithms.jl
+++ src/SamplingBasedPlanningAlgorithms.jl
@@ -3,6 +3,7 @@ module SamplingBasedPlanningAlgorithms
 export Point, Pose, Range
 export Obstacle, CircleObstacle, PolygonObstacle, RectangleObstacle, SquareObstacle, ObstacleType
 export PointAgent, CircleAgent, PolygonAgent
+export World
 
 include("environment.jl")
 include("plotting.jl")
